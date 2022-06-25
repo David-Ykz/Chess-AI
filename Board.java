e06573e419492dc1f7ac46729fdf5ade217be81b
David-Ykz <nerdkid314159@gmail.com>
2022-06-25 00:32:24 -0400

    started fixing errors

---
+++ Board.java
@@ -0,0 +1,251 @@
+import java.awt.*;
+import java.util.*;
+
+class Board {
+    private int turn;
+    private HashMap<Integer, Integer> pieces;
+    private HashMap<Integer, String> pieceNames = new HashMap<>();
+
+        Board(int turn, HashMap<Integer, Integer> pieces) {
+            this.turn = turn;
+            this.pieces = pieces;
+            pieceNames.put(6, "k");
+            pieceNames.put(5, "q");
+            pieceNames.put(4, "r");
+            pieceNames.put(3, "n");
+            pieceNames.put(2, "b");
+            pieceNames.put(1, "p");
+        }
+
+        public int getTurn() {
+            return this.turn;
+        }
+
+        public void changeTurn() {
+            this.turn = turn * -1;
+        }
+
+        public int pieceColor(int position) {
+            if (pieces.get(position) > 0) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+
+        public Integer getPiece(int position) {
+            return this.pieces.get(position);
+        }
+
+        public boolean emptySquare(int position) {
+            return !this.pieces.containsKey(position);
+        }
+
+        public boolean friendlySquare(int position, int color) {
+            return (!emptySquare(position) && pieceColor(position) == color);
+        }
+
+        public boolean hostileSquare(int position, int color) {
+            return (!emptySquare(position) && pieceColor(position) != color);
+        }
+
+        public void changePiecePos(int oldPosition, int newPosition) {
+            pieces.put(newPosition, pieces.remove(oldPosition));
+        }
+
+        public int movePiece(int oldPosition, int newPosition) {
+            if (pieces.get(oldPosition) == 6) { // CHECK IF INTEGER != INT
+                if (oldPosition == 58) {
+                    if (newPosition == 38) { // Queenside castle
+                        castlePiece(oldPosition, newPosition, pieces.get(18), newPosition + 10);
+                        return 0;
+                    } else if (newPosition == 78) { // Kingside castle
+                        castlePiece(oldPosition, newPosition, pieces.get(88), newPosition - 10);
+                        return 0;
+                    }
+                } else if (oldPosition == 51) {
+                    if (newPosition == 31) { // Queenside castle
+                        castlePiece(oldPosition, newPosition, pieces.get(11), newPosition + 10);
+                        return 0;
+                    } else if (newPosition == 71) { // Kingside castle
+                        castlePiece(oldPosition, newPosition, pieces.get(81), newPosition - 10);
+                        return 0;
+                    }
+                }
+            }
+
+
+            int removedPiece = 0;
+            if (pieces.containsKey(newPosition) && pieceColor(oldPosition) != pieceColor(newPosition)) {
+                removedPiece = pieces.remove(newPosition);
+            }
+            changePiecePos(oldPosition, newPosition);
+            return removedPiece;
+        }
+
+        public void castlePiece(int oldKingPos, int newKingPos, int oldRookPos, int newRookPos) {
+            changePiecePos(oldKingPos, newKingPos);
+            changePiecePos(oldRookPos, newRookPos);
+        }
+
+
+
+        public void revertMove(int oldPosition, int newPosition, int capturedPiece) {
+            if (pieces.get(newPosition) == 6) { // CHECK IF INTEGER != INT
+                if (oldPosition == 51) { // Black king
+                    if (newPosition == 31) {
+                        changePiecePos(pieces.get(41), 11); // Queenside castle
+                    } else if (newPosition == 71) {
+                        changePiecePos(pieces.get(61), 81); // Kingside castle
+                    }
+                }
+                if (oldPosition == 58) { // White king
+                    if (newPosition == 38) {
+                        changePiecePos(pieces.get(48), 18); // Queenside castle
+                    } else if (newPosition == 78) {
+                        changePiecePos(pieces.get(68), 88); // Kingside castle
+                    }
+                }
+            }
+            changePiecePos(newPosition, oldPosition);
+            if (capturedPiece != 0) {
+                pieces.put(newPosition, capturedPiece);
+            }
+        }
+
+        public boolean isCheckmate() {
+            int numOfLegalMoves = 0;
+            for (Piece piece : getPieces()) {
+                if (piece.getColor() == turn) {
+                    numOfLegalMoves += piece.findLegalMoves(this).size();
+                }
+            }
+            return numOfLegalMoves == 0;
+        }
+
+        public String toFEN() {
+            String[][] chessboard = new String[8][8];
+            for (Piece piece : getPieces()) {
+                String pieceCharacter = pieceNames.get(piece.getName());
+                if (piece.getColor() == 1) {
+                    pieceCharacter = pieceCharacter.toUpperCase();
+                }
+                chessboard[piece.getPosition() % 10 - 1][piece.getPosition() / 10 - 1] = pieceCharacter;
+            }
+
+            String fen = "";
+
+            for (int i = 0; i < chessboard.length; i++) {
+                String[] row = chessboard[i];
+                int counter = 0;
+                for (int j = 0; j < row.length; j++) {
+                    if (row[j] == null) {
+                        counter++;
+                    } else if (counter == 0) {
+                        fen += row[j];
+                    } else {
+                        fen += counter + row[j];
+                        counter = 0;
+                    }
+                }
+                if (counter != 0) {
+                    fen += counter;
+                }
+                fen += "/";
+            }
+            fen = fen.substring(0, fen.length() - 1);
+            if (this.turn > 0) {
+                fen += " b";
+            } else {
+                fen += " w";
+            }
+            return fen;
+        }
+
+
+
+
+
+        public void drawEvaluation(Graphics g, int GRIDSIZE) {
+            Color whiteGrey = new Color(200, 200, 200);
+            Color blackGrey = new Color(100, 100, 100);
+            double evaluation = evaluateBoard();
+            int evaluationDisplay = (int)(-evaluation * 20);
+            // Black Gauge
+            g.setColor(blackGrey);
+            g.fillRect(GRIDSIZE * 8, 0, 20, evaluationDisplay + GRIDSIZE * 4);
+            // White Gauge
+            g.setColor(whiteGrey);
+            g.fillRect(GRIDSIZE * 8, evaluationDisplay + GRIDSIZE * 4, 20, GRIDSIZE * 4 - evaluationDisplay);
+        }
+        public void drawBoard(Graphics g, int GRIDSIZE) {
+            Color lightSquare = new Color(241, 217, 182);
+            Color darkSquare = new Color(181, 137, 99);
+            Color highlightedLightSquare = new Color(205, 210, 106);
+            Color highlightedDarkSquare = new Color(170, 162, 58);
+            for (int y = 0; y < 8; y++) {
+                for (int x = 0; x < 8; x++) {
+                    if ((x + y) % 2 == 0) {
+                        if (Chess.selectedSquares.contains(10 * (x + 1) + y + 1)) {
+                            g.setColor(highlightedLightSquare);
+                        } else {
+                            g.setColor(lightSquare);
+                        }
+                    } else {
+                        if (Chess.selectedSquares.contains(10 * (x + 1) + y + 1)) {
+                            g.setColor(highlightedDarkSquare);
+                        } else {
+                            g.setColor(darkSquare);
+                        }
+                    }
+                    g.fillRect(x * GRIDSIZE, y * GRIDSIZE, GRIDSIZE, GRIDSIZE);
+                }
+            }
+            for (Piece piece : this.pieces.values()) {
+                piece.drawPiece(g, GRIDSIZE);
+            }
+        }
+
+        // Important methods
+        public double evaluateBoard() {
+            double evaluation = 0;
+            double developmentBoost = 0.1;
+            double pieceActivity = 0.02;
+
+            for (Piece piece : this.pieces.values()) {
+                evaluation += piece.findValue(this) * piece.getColor();
+
+                if (piece.getName().equals("knight") && piece.getPosition() != 1 && piece.getPosition() != 8) {
+                    evaluation += developmentBoost * piece.getColor();
+                } else if (piece.getName().equals("bishop")) {
+                    evaluation += piece.findPossibleMoves(this).size() * pieceActivity * piece.getColor();
+                } else if (piece.getName().equals("queen") && piece.getPosition() != 1 && piece.getPosition() != 8) {
+                    evaluation -= developmentBoost * 2 * piece.getColor();
+                }
+//            System.out.println(piece.getName() + " " + evaluation);
+
+                // Development Index
+//            if (piece.getColor() > 0 && piece.getPosition() % 10 != 1 && !piece.getName().equals("king") && !piece.getName().equals("rook")) {
+                //               evaluation += developmentBoost * piece.getColor();
+                //          } else if (piece.getColor() < 0 && piece.getPosition() % 10 != 8 && !piece.getName().equals("king") && !piece.getName().equals("rook")) {
+                //            evaluation += developmentBoost * piece.getColor();
+                //        }
+
+                //
+
+
+
+            }
+//        System.out.println(evaluation);
+            return evaluation;
+        }
+
+
+
+
+
+
+    }
+
+
+}

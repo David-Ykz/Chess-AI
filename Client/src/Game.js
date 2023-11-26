645f75a8552c70e7b5b720f7004cd5d111dfd4ca
David-Ykz <kzdavid314@gmail.com>
2023-11-25 19:02:28 -0500

    fixed url address

--- Client/src/Game.js
+++ Client/src/Game.js
@@ -72,8 +72,9 @@ function Game({ players, room, orientation, cleanup }) {
     function getData(fen) {
         const backendIP = "3.133.93.27";
         const backendPort = "8080"; // 80, 443
-//        const url = `https://${backendIP}:${backendPort}/move`
-        const url = "https://main.d3kqvs59i8mifl.amplifyapp.com/move";
+        const url = `http://${backendIP}:${backendPort}/move`
+//        //const url = "https://main.d3kqvs59i8mifl.amplifyapp.com/move";
+    //    const url = "https://ec2-3-133-93-27.us-east-2.compute.amazonaws.com/move";
         axios.post(url, fen)
             .then(response => {
                 console.log(response.data);
